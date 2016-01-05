9bc6806ae546becfc6e2a5b1a84bccb7bba27bec
Anton <romanukhaav@gmail.com>
2016-01-06 01:10:59 +0200

    Create 13_1_Sum.java

---
+++ 13_1_Sum.java
@@ -0,0 +1,58 @@
+package lesson13;
+/* Посчитать сумму двух чисел и записать в третье.
+ * (Используя AWT, Generics, классы-обертки и обработку ошибок)*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Sum extends JFrame {
+	private static final long serialVersionUID = 1L; // ще не розумію навіщо то
+
+	private void updateCounter() {
+	   countLabel.setText("  Результат = "+result);
+	}
+	
+    private Integer result = 0;
+	private JLabel countLabel;
+	private JButton addNum;
+	private TextField num1;
+	private TextField num2;
+	
+	public Sum(){
+	    super("Сумма двух чисел");
+	    
+	    //Подготавливаем компоненты объекта
+	    countLabel = new JLabel("  ");
+	    addNum = new JButton("Add");
+	    num1 = new TextField("Введите первое число", 15);
+	    num2 = new TextField("Введите второе число", 15);
+
+	    //Подготавливаем временные компоненты
+	    JPanel buttonsPanel = new JPanel(new FlowLayout());
+	    
+	    //Расставляем компоненты по местам
+	    add(num1, BorderLayout.NORTH);
+	    add(num2, BorderLayout.CENTER);
+	    buttonsPanel.add(addNum);
+	    buttonsPanel.add(countLabel);
+	    
+	    add(buttonsPanel, BorderLayout.SOUTH);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  	
+	    // добавляем слушатель кнопки и обработчик события
+	    addNum.addActionListener(new ActionListener(){
+    	  public void actionPerformed(ActionEvent e) {
+    		  result = Integer.parseInt(num1.getText())+Integer.parseInt(num2.getText());
+    		  updateCounter(); //Сообщаем приложению результат
+    	  }
+    	});
+     
+	}
+	  
+	public static void main (String[] args) {
+		Sum app = new Sum();
+	    app.setVisible(true);
+	    app.setSize(300,130);
+	  }
+}
