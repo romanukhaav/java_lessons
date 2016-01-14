22300b887ede99a586121e5d86a72da960e1b4ab
Anton <romanukhaav@gmail.com>
2016-01-15 00:55:42 +0200

    Update 14_1_StrToArray.java

--- 14_1_StrToArray.java
+++ 14_1_StrToArray.java
@@ -32,7 +32,7 @@ public class StrToArray {
 			arr[i]=myStr.toCharArray()[i];
 		
 		// вычисляем колличество занятых, свободных элементов и размер массива и выводим результаты
-		System.out.print("Получен массив");
+		System.out.print("\nПолучен массив ");
 		for ( int i = 0; i<arr.length; i++)
 			if (arr[i]!='\0')	System.out.print(arr[i]+" "); // '\0' - пустой символ
 			else System.out.print("_ ");
