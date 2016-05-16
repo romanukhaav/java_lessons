1547266d83996c4dcdfd8e41417aef4c44322ab8
Anton <romanukhaav@gmail.com>
2016-05-17 00:32:58 +0300

    Create 23_1_MyArrayList.java

---
+++ 23_1_MyArrayList.java
@@ -0,0 +1,23 @@
+package lesson23;
+
+/*Создайте класс динамического массива, в котором реализована проверка выхода за границы массива.
+ * Перегрузите операторы: 
+ * add(int, Integer) - добавление элемента в конкретное место массива 
+ * add(Integer) - добавление элемента в конец массива
+ * del(Integer) - удаление элемента из конца массива
+ * del(int, Integer) - удаление элемента из конкретного места массива.
+ */
+
+public class MyArrayList extends java.util.ArrayList <Integer> {
+
+	public void add(){
+	
+	
+	
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
